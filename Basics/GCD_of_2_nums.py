4f52e36210b40cececa7198e3b437b1511138bcd
kalaiyarazan <kalaiyarazan.v@gmail.com>
2019-09-23 11:50:00 +0530

    greatest common divisor- Clarification

--- Basics/GCD_of_2_nums.py
+++ Basics/GCD_of_2_nums.py
@@ -6,3 +6,13 @@ for i in range(1,int(min_inp)+1):
     if(int(inp[0])%i==0 and int(inp[1])%i==0):
         gcd=i
 print(gcd)
+# clarification: unable to print "-1"
+
+
+# 18
+# Given 2 numbers N,M find the GCD of N and M.If it cannot be found for given number(s) then print -1
+# Sample Testcase :
+# INPUT
+# 10 5
+# OUTPUT
+# 5
