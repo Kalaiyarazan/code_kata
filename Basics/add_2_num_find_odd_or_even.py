dba6e2ae4c784ce7a40ae963f52c86a674a505d9
kalaiyarazan <kalaiyarazan.v@gmail.com>
2019-09-23 12:02:25 +0530

    add_2_num_find_odd_or_even

---
+++ Basics/add_2_num_find_odd_or_even.py
@@ -0,0 +1,19 @@
+nm_input=input()
+nm_input_array=nm_input.split(" ")
+n=int(nm_input_array[0])
+m=int(nm_input_array[1])
+sum=n+m
+if sum%2==0:
+    print("even")
+else:
+    print("odd")
+
+
+
+# 1
+# Given 2 numbers N and M add both the numbers and check whether the sum is odd or even.
+# Sample Testcase :
+# INPUT
+# 9 2
+# OUTPUT
+# odd
